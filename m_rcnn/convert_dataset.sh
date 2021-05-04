f5ed8b7e937d9b84383f55f035e30a7a1a3b3ca0
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-05-03 19:06:25 -0500

    [FIX] Added lib for opencv

--- m_rcnn/convert_dataset.sh
+++ m_rcnn/convert_dataset.sh
@@ -1,20 +1,10 @@
 python ../research/object_detection/dataset_tools/create_coco_tf_record.py \
 --logtostderr \
---train_image_dir=datasets/620_red_yellow_cart_only/train \
---val_image_dir=datasets/620_red_yellow_cart_only/val \
---test_image_dir=datasets/620_red_yellow_cart_only/val \
---train_annotations_file=datasets/620_red_yellow_cart_only/train/via_region_data_coco.json \
---val_annotations_file=datasets/620_red_yellow_cart_only/val/via_region_data_coco.json \
---testdev_annotations_file=datasets/620_red_yellow_cart_only/val/via_region_data_coco.json \
+--train_image_dir=620_red_yellow_cart_only/train \
+--val_image_dir=620_red_yellow_cart_only/val \
+--test_image_dir=620_red_yellow_cart_only/val \
+--train_annotations_file=620_red_yellow_cart_only/train/496_images_coco.json \
+--val_annotations_file=620_red_yellow_cart_only/val/124_images_coco.json \
+--testdev_annotations_file=620_red_yellow_cart_only/val/124_images_coco.json \
 --include_masks=True \
---output_dir=datasets/tensorflow
-
-
-# python create_coco_tf_record.py 
-# --logtostderr 
-# --train_image_dir=images/train 
-# --test_image_dir=images/test 
-# --train_annotations_file=images/train.json 
-# --test_annotations_file=images/test.json 
-# --include_masks=True 
-# --output_dir=./
+--output_dir=tensorflow
